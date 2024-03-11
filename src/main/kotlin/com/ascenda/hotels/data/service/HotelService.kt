b2acb2a223fd7156622019a754dcb9471878757c
Hung Duong <hungduong150698@gmail.com>
2024-03-11 22:35:11 +0700

    HotelService run all query in asynchronous style instead of wait for each

--- src/main/kotlin/com/ascenda/hotels/data/service/HotelService.kt
+++ src/main/kotlin/com/ascenda/hotels/data/service/HotelService.kt
@@ -6,6 +6,8 @@ import com.ascenda.hotels.data.service.converter.PaperFlyHotelConverter
 import com.ascenda.hotels.data.service.converter.PatagoniaHotelConverter
 import com.ascenda.hotels.data.service.merger.HotelMerger
 import com.ascenda.hotels.data.service.model.Hotel
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 import org.springframework.stereotype.Service
 
 @Service
@@ -16,12 +18,14 @@ class HotelService(
     private val paperFlyHotelConverter: PaperFlyHotelConverter,
     private val hotelMerger: HotelMerger
 ) {
-    suspend fun getHotels(): List<Hotel> {
-        return hotelMerger.merge(
-            hotelSupplierClient.getAcmeHotels().map { acmeHotelConverter.convert(it) } +
-                    hotelSupplierClient.getPatagoniaHotels().map { patagoniaHotelConverter.convert(it) } +
-                    hotelSupplierClient.getPaperFlyHotels().map { paperFlyHotelConverter.convert(it) }
-        )
+    suspend fun getHotels(): List<Hotel> = coroutineScope {
+        val acmeHotels = async { hotelSupplierClient.getAcmeHotels().map { acmeHotelConverter.convert(it) } }
+        val patagoniaHotels =
+            async { hotelSupplierClient.getPatagoniaHotels().map { patagoniaHotelConverter.convert(it) } }
+        val paperFlyHotels =
+            async { hotelSupplierClient.getPaperFlyHotels().map { paperFlyHotelConverter.convert(it) } }
+        hotelMerger.merge(acmeHotels.await() + patagoniaHotels.await() + paperFlyHotels.await())
     }
+
 }
 
--- src/test/kotlin/com/ascenda/hotels/data/unit/service/HotelServiceTests.kt
+++ src/test/kotlin/com/ascenda/hotels/data/unit/service/HotelServiceTests.kt
@@ -13,6 +13,7 @@ import com.ascenda.hotels.data.service.converter.AcmeHotelConverter
 import com.ascenda.hotels.data.service.converter.PaperFlyHotelConverter
 import com.ascenda.hotels.data.service.converter.PatagoniaHotelConverter
 import com.ascenda.hotels.data.service.merger.HotelMerger
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.equalTo
@@ -21,7 +22,9 @@ import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.InjectMocks
 import org.mockito.Mock
 import org.mockito.junit.jupiter.MockitoExtension
+import org.mockito.kotlin.doSuspendableAnswer
 import org.mockito.kotlin.whenever
+import java.time.Instant.now
 
 @ExtendWith(MockitoExtension::class)
 class HotelServiceTests {
@@ -59,28 +62,28 @@ class HotelServiceTests {
     }
 
 
-//    @Test
-//    fun `should query sources asynchronously`() {
-//        whenever(hotelSupplierClient.getAcmeHotels()).thenAnswer{
-//            Thread.sleep(1000)
-//            listOf(acmeBeachVilla)
-//        }
-//        whenever(hotelSupplierClient.getPatagoniaHotels()).thenAnswer{
-//            Thread.sleep(2000)
-//            listOf(patagoniaBeachVilla)
-//        }
-//        whenever(hotelSupplierClient.getPaperFlyHotels()).thenAnswer{
-//            Thread.sleep(3000)
-//            listOf(paperFlyBeachVilla)
-//        }
-//        whenever(acmeHotelConverter.convert(acmeBeachVilla)).thenReturn(hotelAcmeBeachVilla)
-//        whenever(patagoniaHotelConverter.convert(patagoniaBeachVilla)).thenReturn(hotelPatagoniaBeachVilla)
-//        whenever(paperFlyHotelConverter.convert(paperFlyBeachVilla)).thenReturn(hotelPaperFlyBeachVilla)
-//        whenever(hotelMerger.merge(listOf(hotelAcmeBeachVilla, hotelPatagoniaBeachVilla, hotelPaperFlyBeachVilla))).thenReturn(listOf(hotelBeachVilla))
-//        val start = now()
-//        val actual = service.getHotels()
-//
-//        assertThat(actual, equalTo(listOf(hotelBeachVilla)))
-//        assertThat(now().toEpochMilli() - start.toEpochMilli() < 4000, equalTo(true))
-//    }
+    @Test
+    fun `should query sources asynchronously`() = runBlocking {
+        whenever(hotelSupplierClient.getAcmeHotels()).doSuspendableAnswer{
+            delay(1000)
+            listOf(acmeBeachVilla)
+        }
+        whenever(hotelSupplierClient.getPatagoniaHotels()).doSuspendableAnswer{
+            delay(2000)
+            listOf(patagoniaBeachVilla)
+        }
+        whenever(hotelSupplierClient.getPaperFlyHotels()).doSuspendableAnswer{
+            delay(3000)
+            listOf(paperFlyBeachVilla)
+        }
+        whenever(acmeHotelConverter.convert(acmeBeachVilla)).thenReturn(hotelAcmeBeachVilla)
+        whenever(patagoniaHotelConverter.convert(patagoniaBeachVilla)).thenReturn(hotelPatagoniaBeachVilla)
+        whenever(paperFlyHotelConverter.convert(paperFlyBeachVilla)).thenReturn(hotelPaperFlyBeachVilla)
+        whenever(hotelMerger.merge(listOf(hotelAcmeBeachVilla, hotelPatagoniaBeachVilla, hotelPaperFlyBeachVilla))).thenReturn(listOf(hotelBeachVilla))
+        val start = now()
+        val actual = service.getHotels()
+
+        assertThat(actual, equalTo(listOf(hotelBeachVilla)))
+        assertThat(now().toEpochMilli() - start.toEpochMilli() < 4000, equalTo(true))
+    }
 }
